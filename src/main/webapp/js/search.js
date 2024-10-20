60d281ce0ce1370eddde19f912d691d0658f95ad
K <K@DESKTOP-G15330L>
2024-10-20 19:02:44 +0900

    검색 js 오류 이슈 수정

--- src/main/webapp/js/search.js
+++ src/main/webapp/js/search.js
@@ -1,22 +1,29 @@
 window.addEventListener('DOMContentLoaded', function() {
 	// 검색 폼 select active
 	const _select = document.querySelector('.search_input .select_type');
-	_select.addEventListener('click', function() {
-		_select.classList.toggle('on');
-	});
+	if (_select) {
+		_select.addEventListener('click', function() {
+			_select.classList.toggle('on');
+		});
+	}
 	
 	// 위시리스트 active
 	const _wish = document.querySelector('.thumbnail .wish');
-	_wish.addEventListener('click', function() {
-		_wish.classList.toggle('active');
-	})	
+	if (_wish) {
+		_wish.addEventListener('click', function() {
+			_wish.classList.toggle('active');
+		});	
+	}	
 	
 	// 검색 영역
 	const _topInput = document.querySelector('.txt_field .text_keyword');
-	_topSearch.addEventListener('click', function(e) {
-		if (_topInput.value.length === 0 ) {
-			e.preventDefault();
-			alert('검색어를 입력하세요.');
-		};
-	});
+	const _topSearch = document.querySelector('.txt_field .search_button'); // 검색 버튼 선택
+	if (_topSearch && _topInput) {
+		_topSearch.addEventListener('click', function(e) {
+			if (_topInput.value.length === 0) {
+				e.preventDefault();
+				alert('검색어를 입력하세요.');
+			}
+		});
+	}
 });
