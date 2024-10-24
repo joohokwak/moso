eb6be914dfc15cc4246bc46591698c21d3400d93
tj <tj@DESKTOP-LNVM8UR>
2024-10-24 18:09:57 +0900

    검색 페이지 페이징 처리 수정

--- src/main/java/controller/QuizController.java
+++ src/main/java/controller/QuizController.java
@@ -8,10 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import common.Pagination;
-import dto.MemberDTO;
 import dto.QuizDTO;
 import service.QuizService;
 import serviceImpl.QuizServiceImpl;
@@ -24,19 +22,11 @@ public class QuizController extends HttpServlet {
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("utf-8");
 		resp.setContentType("text/html; charset=utf-8");
-
-		// 세션에서 로그인 정보 가져오기
-		HttpSession session = req.getSession();
-		Object user = session.getAttribute("member");
-		String id = null;
-		if (user != null) {
-			MemberDTO member = (MemberDTO) user;
-			id = member.getId();
-		}
-
+		
 		String action = req.getPathInfo();
+		
 		QuizService qs = new QuizServiceImpl();
-
+		
 		// 결과 페이지
 		if (action.equals("/result")) {
 			String q1 = req.getParameter("Q1");
@@ -44,11 +34,11 @@ public class QuizController extends HttpServlet {
 			String q3 = req.getParameter("Q3");
 			String q4 = req.getParameter("Q4");
 			String sumQ = q1 + q2 + q3 + q4;
-
+			
 			List<QuizDTO> list = qs.mattressQuiz(sumQ);
-
+			
 			req.setAttribute("quiz", list);
-
+			
 		// search 페이지
 		} else if (action.equals("/search")) {
 			String headerSearch = req.getParameter("name");
@@ -57,13 +47,17 @@ public class QuizController extends HttpServlet {
 			String sort = req.getParameter("sort");
 			
 			if (sort == null) sort = "visit_desc";
-
+			
 			if (headerSearch != null) {
 				key = "goodsName";
 				keyword = headerSearch;
 			}
-
+			
+			String strNum = req.getParameter("pageNum");
+			int pageNum = strNum != null ? Integer.parseInt(strNum) : 1;
+			
 			Pagination pg = new Pagination();
+			pg.setPageNum(pageNum);
 			pg.setPageSize(12);
 			
 			List<QuizDTO> searchAll = qs.setGoods(pg, key, keyword, sort);
